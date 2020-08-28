a67374088684391c3d7491d771c2f52bf5481cbe
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-08-28 22:10:12 +0200

    folder structure

--- build.bat
+++ build.bat
@@ -1,3 +1,16 @@
+@echo off
 cd ./
 echo Build Started
+
+echo Packing Resourcepack
+
+cd resourcepack
+
+winrar a -afzip ../resourcepack.zip assets
+winrar a -afzip ../resourcepack.zip pack.mcmeta
+
+cd ../
+echo Building Functions
+
 mcscript compile
+
